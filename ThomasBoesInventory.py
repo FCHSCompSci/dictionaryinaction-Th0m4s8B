f128f5c26f1c6757ee6db0aaee2ea8b183df9b95
Thomas Bailey Boes <thomas.boes@stu.jefferson.kyschools.us>
2018-09-18 08:35:33 -0400

    added pick up and check abilities

--- ThomasBoesInventory.py
+++ ThomasBoesInventory.py
@@ -12,16 +12,28 @@ def inventory():
         print("You can also quit")
         action = input("What do you choose to do?(pickup/check/quit): ")
 
-        
+        #picks up an item
         if action == "pickup":
-            print("There is a %s in front of you." %s(items[0]))
-            input("What do you want to pick up?(%s): " %s(items[0]))
+            print("There is an apple, a coin, a sentient pile of dirt,and 50 copies of skyrim in front of you.")
+            new_item = input("What do you want to pick up?(apple/coin/dirt/skyrim): ")
+            print("You pickup %s." %(new_item))
+            #checks if a slot is empty and can hold an item
+            if storage['space_1'] == "empty":
+                storage['space_1'] = new_item
+            else:
+                if storage['space_2'] == "empty":
+                    storage['space_2'] = new_item
+                else:
+                    if storage['space_3'] == "empty":
+                        storage['space_3'] = new_item
 
-            
+                        
+        #shows the inventory    
         elif action == "check":
-            break
+            print(storage)
 
-        
+
+        #ends the loop
         elif action == "quit":
             print("You close your inventory.")
             break
