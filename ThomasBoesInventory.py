36d4e9663a5bdd96599a456737f92cf7fac26af8
Thomas Bailey Boes <thomas.boes@stu.jefferson.kyschools.us>
2018-09-19 08:35:45 -0400

    added inventory expansion option

--- ThomasBoesInventory.py
+++ ThomasBoesInventory.py
@@ -9,32 +9,38 @@ def inventory():
     while True:
         items = ["apple","coin","sentient pile of dirt","50 copies of skyrim"]
         print("You can pick up an item or check your inventory.")
+        print("You can also increase or decrease your inventory size.")
         print("You can also quit")
-        action = input("What do you choose to do?(pickup/check/quit): ")
+        action = input("What do you choose to do?(pickup/check/quit/(e)xpand_inventory): ")
 
         #picks up an item
         if action == "pickup":
-            print("There is an apple, a coin, a sentient pile of dirt,and 50 copies of skyrim in front of you.")
+            print()
+            print("There are apples, coins, sentient piles of dirt,and many copies of skyrim in front of you.")
             new_item = input("What do you want to pick up?(apple/coin/dirt/skyrim): ")
+            print()    
             print("You pickup %s." %(new_item))
             #checks if a slot is empty and can hold an item
-            if storage['space_1'] == "empty":
-                storage['space_1'] = new_item
-            else:
-                if storage['space_2'] == "empty":
-                    storage['space_2'] = new_item
-                else:
-                    if storage['space_3'] == "empty":
-                        storage['space_3'] = new_item
-
-                        
+            for key in storage:
+                if storage[key] == "empty":
+                    storage[key] = new_item
+                    break
+                    
+            
         #shows the inventory    
         elif action == "check":
-            print(storage)
-
+            for key, value in storage.items():
+                print()
+                print("%s: %s" % (key, value))
+                print()
+        #increases number of inventory spaces
+        elif action == "e":
+            storage['space_%s'%(len(storage)+1)] = "empty"
+            print("Your inventory has gained a new space.")
 
         #ends the loop
         elif action == "quit":
             print("You close your inventory.")
             break
                     
+
