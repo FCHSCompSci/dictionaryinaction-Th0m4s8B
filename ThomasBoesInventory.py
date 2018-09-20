616e0eca2d697b89fd592ff7eb983fece0f7002f
Thomas Bailey Boes <thomas.boes@stu.jefferson.kyschools.us>
2018-09-20 08:33:20 -0400

    added new user interface

--- ThomasBoesInventory.py
+++ ThomasBoesInventory.py
@@ -11,10 +11,10 @@ def inventory():
         print("You can pick up an item or check your inventory.")
         print("You can also increase or decrease your inventory size.")
         print("You can also quit")
-        action = input("What do you choose to do?(pickup/check/quit/(e)xpand_inventory): ")
+        action = input("What do you choose to do?((p)ickup/(c)heck/(q)uit/(e)xpand_inventory): ")
 
         #picks up an item
-        if action == "pickup":
+        if action == "p":
             print()
             print("There are apples, coins, sentient piles of dirt,and many copies of skyrim in front of you.")
             new_item = input("What do you want to pick up?(apple/coin/dirt/skyrim): ")
@@ -25,22 +25,33 @@ def inventory():
                 if storage[key] == "empty":
                     storage[key] = new_item
                     break
+                
+                #need to put this into my code
+                    print("----------------------")
+                    print("Your inventory is full")
+                    print("----------------------")
                     
-            
+
         #shows the inventory    
-        elif action == "check":
+        elif action == "c":
             for key, value in storage.items():
                 print()
                 print("%s: %s" % (key, value))
-                print()
+            print()
+            
         #increases number of inventory spaces
         elif action == "e":
             storage['space_%s'%(len(storage)+1)] = "empty"
+            print("--------------------------------------")
             print("Your inventory has gained a new space.")
+            print("--------------------------------------")
 
+            
         #ends the loop
-        elif action == "quit":
+        elif action == "q":
+            print("-------------------------")
             print("You close your inventory.")
+            print("-------------------------")
             break
                     
 
